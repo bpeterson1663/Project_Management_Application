179a49b054cdba395e793db25c694aa34eb25b4b
bpeterson1663 <bpeterson1663@gmail.com>
2016-03-11 13:02:05 -0600

    We Poofed Again

--- client/app.js
+++ client/app.js
@@ -3,13 +3,20 @@ var staff = {};
 var maxFrontEndPoints = 60;
 var maxClientPoints = 60;
 var maxServerPoints = 60;
+
 var clientLogicFilled = false;
 var frontEndFilled = false;
 var serverLogicFilled = false;
+
 var positionFilled = 0;
+
 var frontEndScrum = 0;
 var clientsideScrum = 0;
 var serverScrum = 0;
+
+var sprintsArray = [];
+var totalSprints = 0;
+
 
 $(document).ready(function(){
   $('body').prepend('<button class="generate">Generate Project</button>');
@@ -36,6 +43,7 @@ function createProject(){
   //$('.container').append('<div class="projectContainer"></div>');
   $('.projectContainer').append('<ul><h2>' + randomCompanyGenerator() + '</h2><li>Front-End Scrum Points: ' + frontEndScrum + '</li><li>Clientside Scrum Points: ' + clientsideScrum +'</li><li>Serverside Scrum Points: '+ serverScrum +'</li></ul>');
   $('.projectContainer').append('<button class="assignStaff">Assign Staff</button>');
+
 }
 
 var randomCompanyGenerator = function (){
@@ -59,17 +67,26 @@ function randomNumber(min, max){
 }
 
 function assignStaff(){
-        $.ajax({
-          type: 'GET',
-          url: '/staff',
-          success: function(data){
-            staff.name = data.name;
-            staff.skill = data.skill;
-            staff.sprint = data.sprint;
-            matchSkills(staff);
-          }
-        });
+  $.ajax({
+    type: 'GET',
+    url: '/staff',
+    success: function(data){
+      staff.name = data.name;
+      staff.skill = data.skill;
+      staff.sprint = data.sprint;
+      matchSkills(staff);
+    }
+  });
 
+
+  // for (var i = 0; i < sprintsArray.length; i++) {
+  //   totalSprints += sprintsArray[i];
+  // }
+
+  //$('.projectContainer').append('<p>Weeks until completion: ' + (totalSprints / sprintsArray.length) + '</p>');
+
+  // console.log(totalSprints);
+  // console.log(sprintsArray);
 }
 
 function matchSkills(staff){
@@ -80,9 +97,11 @@ function matchSkills(staff){
         $('.clientside-container').append('<h2>' + staff.name + '</h2>');
         $('.clientside-container').append('<p>Skill: ' + staff.skill + '</p>');
         $('.clientside-container').append('<p>Average Sprints: ' + staff.sprint + '</p>');
-        $('.clientside-container').append('<p>Sprints till completion: ' + Math.ceil(clientsideScrum / staff.sprint) + '</p>');
+        $('.clientside-container').append('<p class="clientside-completion">Sprints till completion: ' + Math.ceil(clientsideScrum / staff.sprint) + '</p>');
+        $('.clientside-completion').data('sprints', Math.ceil(clientsideScrum / staff.sprint));
+        console.log($('.clientside-completion').data());
 
-
+        sprintsArray.push($('.clientside-completion').data('sprints'));
 
         clientLogicFilled = true;
         console.log(staff.skill , "filled!");
@@ -93,7 +112,10 @@ function matchSkills(staff){
         $('.frontend-container').append('<h2>' + staff.name + '</h2>');
         $('.frontend-container').append('<p>Skill: ' + staff.skill + '</p>');
         $('.frontend-container').append('<p>Average Sprints: ' + staff.sprint + '</p>');
-        $('.frontend-container').append('<p>Sprints till completion: ' + Math.ceil(frontEndScrum / staff.sprint) + '</p>');
+        $('.frontend-container').append('<p class="frontend-completion">Sprints till completion: ' + Math.ceil(frontEndScrum / staff.sprint) + '</p>');
+        $('.frontend-completion').data('sprints', Math.ceil(frontEndScrum / staff.sprint));
+        sprintsArray.push($('.frontend-completion').data('sprints'));
+
 
         // $('.frontend-container').append('<p>''</p>');
         frontEndFilled = true;
@@ -105,7 +127,9 @@ function matchSkills(staff){
           $('.serverside-container').append('<h2>' + staff.name + '</h2>');
           $('.serverside-container').append('<p>Skill: ' + staff.skill + '</p>');
           $('.serverside-container').append('<p>Average Sprints: ' + staff.sprint + '</p>');
-          $('.serverside-container').append('<p>Sprints till completion: ' + Math.ceil(serverScrum / staff.sprint) + '</p>');
+          $('.serverside-container').append('<p class="serverside-completion">Sprints till completion: ' + Math.ceil(serverScrum / staff.sprint) + '</p>');
+          $('.serverside-completion').data('sprints', Math.ceil(serverScrum / staff.sprint));
+          sprintsArray.push($('.serverside-completion').data('sprints'));
 
           serverLogicFilled = true;
           console.log(staff.skill , "filled!");
@@ -115,11 +139,15 @@ function matchSkills(staff){
         staff = {};
         assignStaff();
       }
+      if(positionFilled == 3){
+        for (var i = 0; i < sprintsArray.length; i++) {
+          totalSprints += sprintsArray[i];
+        }
+        $('.projectContainer').children('.assignStaff').after('<p>Weeks until completion: ' + Math.ceil((totalSprints / sprintsArray.length)) + '</p>');
+        console.log(totalSprints);
+        console.log(sprintsArray);
+      }
 
     // console.log("i = " + i);
   // }
-}
-
-function appendEmployee() {
-
 }
